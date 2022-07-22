c88622cdafa1bc3f8c83ce6274918719bc1c9f53
wahi90 <mdshahed1098@gmail.com>
2022-07-22 11:34:55 +0600

    first commit

---
+++ index.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+  
+ int a,b;
+ 
+ printf("enter a");
+ scanf("%d", &a);
+ printf ("enter b");
+ scanf("%d",&b);
+
+ int sum = a * b;
+
+ printf("total is: %d", sum);
+   
+    
+    return 0;
+}
+
+/* Variable Rules   
+1. int = to write integer value Ex: 1,3,4
+2. float = to write float value Ex: 1.3,4.3
+3. char = to write Character value Ex: #,*,A
+*/ 
